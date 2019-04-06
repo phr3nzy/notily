212f6afcb2cca9cc6467a1a19ad0cf8e0abb0ba1
phr3nzy <adilosama47@gmail.com>
2019-04-06 21:14:44 +0200

    feat: add sidebar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,42 +1,43 @@
 <template>
   <div>
-    <div>
       <vs-navbar
         v-model="activeIndex"
         color="dark"
+        :collapse="false"
         text-color="rgba(255,255,255,.6)"
         active-text-color="rgba(255,255,255,1)"
       >
         <div slot="title">
-          <vs-navbar-title color="white">Notily</vs-navbar-title>
-        </div>
-        <vs-navbar-item index="0">
-          <vs-button
-            to="/"
-            color-text="white"
-            color="rgba(255, 255, 255, 0.3)"
-            type="flat"
-            icon="home"
-          >Home</vs-button>
-        </vs-navbar-item>
-        <vs-navbar-item index="1">
-          <vs-button
-            to="/notes"
-            color-text="white"
-            color="rgba(255, 255, 255, 0.3)"
-            type="flat"
-            icon="edit"
-          >Notes</vs-button>
-        </vs-navbar-item>
-        <vs-spacer></vs-spacer>
         <vs-button
           color-text="rgb(255, 255, 255)"
           color="rgba(255, 255, 255, 0.3)"
-          type="flat"
-          icon="more_horiz"
+          type="default"
+          icon="menu"
+          @click="active = !active"
         ></vs-button>
+        </div>
       </vs-navbar>
-    </div>
+   
+  <div ref="parentSidebar" id="parentx">
+    <vs-sidebar  :parent="$refs.parentSidebar" default-index="1"  color="primary" class="sidebarx" spacer v-model="active">
+
+      <!-- <div class="header-sidebar" slot="header">
+      </div> -->
+
+      <vs-sidebar-item to="/" index="1" icon="home">
+        Home
+      </vs-sidebar-item>
+
+      <vs-sidebar-item to="/notes" index="2" icon="edit">
+        Notes
+      </vs-sidebar-item>
+
+      <div class="footer-sidebar" slot="footer">
+        <span>Notily&copy; | All Rights Reserved </span>
+      </div>
+
+    </vs-sidebar>
+  </div>
     <div>
       <nuxt/>
     </div>
@@ -46,7 +47,8 @@
 <script>
 export default {
 	data: () => ({
-		activeIndex: 0
+		activeIndex: 0,
+		active:false,
 	})
 };
 </script>

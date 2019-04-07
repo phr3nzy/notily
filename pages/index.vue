5988aedafeb776fd7921c75c9a25f21985820f4c
phr3nzy <adilosama47@gmail.com>
2019-04-07 09:05:47 +0200

    feat: add note taking boilerplate

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,52 @@
 <template>
-  <vs-row id="view" vs-justify="center" vs-align="center">
-    <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="6">
-      <vs-card class="cardx">
-        <div slot="header">
-          <h3>Welcome to Notily!</h3>
-        </div>
-        <div slot="media">
-          <img>
-        </div>
-        <div>
-          <span>The open source note taking tool</span>
-        </div>
-        <div slot="footer">
-          <vs-row vs-justify="center">
-            <vs-button color="primary" type="relief" icon="add">Create First Note!</vs-button>
+  <div style="margin-top: 10%;">
+    <div>
+      <vs-row id="view" vs-justify="center" vs-align="center">
+      <vs-col type="flex" vs-justify="center" vs-align="center" vs-w="4">
+        <vs-card actionable class="cardx">
+          <div slot="header">
+            <h3>Welcome to Notily!</h3>
+          </div>
+          <div slot="media">
+            <img>
+          </div>
+          <div>
+            <span>The open source note taking tool</span>
+          </div>
+          <div slot="footer">
+            <vs-row vs-justify="center">
+              <vs-button @click="popupActive=true" color="primary" type="relief" icon="add">Create a Note!</vs-button>
+            </vs-row>
+          </div>
+        </vs-card>
+      </vs-col>
+    </vs-row>
+    </div>
+    <div class="centerx">
+      <vs-popup :background-color-popup="popupColor" title="Add your note!" :active.sync="popupActive">
+        <vs-card>
+          <vs-input label-placeholder="Note Title" v-model="noteTitle"/>
+          <vs-divider style="color: transparent; background-color: transparent;"></vs-divider>
+          <vs-textarea height="500px" width="100%" label="Note Body" v-model="noteBody"/>
+          <vs-row vs-justify="flex-end">
+            <vs-button size="large" type="relief" icon="save">Save</vs-button>
           </vs-row>
-        </div>
-      </vs-card>
-    </vs-col>
-  </vs-row>
+        </vs-card> 
+      </vs-popup>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-// 	mounted() {
-// 		this.$vs.loading();
-// 		setTimeout(() => {
-// 			this.$vs.loading.close();
-// 		}, 3000);
-// 	}
+	data() {
+		return {
+			popupActive:false,
+			noteTitle: '',
+			noteBody: '',
+			popupColor: 'white'
+		}
+	}
 };
 </script>
 
